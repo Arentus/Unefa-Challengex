487a23d61aec74800d0846f37c1da76b67ebd78b
Robwert Mota <robwert7991@gmail.com>
2018-04-24 15:33:01 -0400

    [HTML][CSS] Created True/False View

---
+++ views/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Challenge Question Game | Unefa | </title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link href="https://fonts.googleapis.com/css?family=Jua" rel="stylesheet">
+</head>
+<body>
+	<button id="reset" class="rebutton button true-button">Reiniciar</button>
+	
+	<div class="counter">
+		0 pts	
+	</div>
+
+	<div class="container">
+
+		<div class="wrapper">
+			<h1>Responde la siguiente pregunta:</h1>
+			
+			<div class="questionwrapper">
+				<h3 class="titlequestion">¿El 4k del ps4 pro en god of war 4 es Nativo?</h3>
+			</div>
+			
+				<div class="buttonwrapper">
+					<hr>
+					<div id="trueq" class="button true-button">
+					True
+					</div>
+					
+					<div id="falseq" class="button false-button">
+					Falso
+					</div>
+				</div>
+
+		</div>
+
+	</div>
+	<!--
+	<div class="marker">
+		<table>
+			<tr class="markersign">
+				<td colspan="2">Puntaje</td>
+			</tr>
+			<tr class="markersign">
+				<td>0</td>
+				<td>0</td>
+			</tr>
+		</table>
+	</div>
+	-->
+</body>
+</html>
